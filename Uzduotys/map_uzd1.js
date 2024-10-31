f3eeb736309a9459b3b37faf2fa5b18e700a5c9e
Henryk <henryk.umbrasevic@stud.techin.lt>
2024-10-31 12:17:04 +0400

    1st map task done

---
+++ Uzduotys/map_uzd1.js
@@ -0,0 +1,19 @@
+const names = ['Alice', 'Bob', 'Charlie', 'Daisy', 'Eve', 'Frank', 'Grace']
+
+// Without using map, create an array with names all in upper case:
+const namesUpperCaseMapped = names.map(name => name.toUpperCase())
+
+/* ===============================C=O=D=E================================= */
+
+const namesUpperCase = [];
+for (let i = 0; i < names.length; i++){
+    const makeCapitals = names[i].toUpperCase()
+    namesUpperCase.push(makeCapitals)
+}
+console.log(namesUpperCase);
+
+/* ===============================T=E=S=T================================= */
+
+const assert = require("node:assert")
+assert.strictEqual(JSON.stringify(namesUpperCase), JSON.stringify(namesUpperCaseMapped));
+console.log("Well done!")
